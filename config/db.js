c92557f2633a87558867a4c69e381add38100680
ziyuw131 <ziyu.wang97@myhunter.cuny.edu>
2024-12-15 23:24:30 -0500

    update files

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ziyuw131:8CkUblXfhqppVIuL@cluster0.c2dlu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://ziyuwang131:6N4N70kzdhdTaGAH@cluster0.bi1xd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
