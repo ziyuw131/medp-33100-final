2f6e659e9823267859afed7f4a909f147c0eea6f
ziyuw131 <ziyu.wang97@myhunter.cuny.edu>
2024-12-17 22:06:21 -0500

    update: first post draft

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  // res.render('index', { title: 'Memoirs of a Melody' });
+  try {
+    const db = req.app.locals.db;
+    const posts = await db.collection('songs')
+      .find()
+      .toArray();
+    console.log(posts);
+
+    res.render('index', { title: 'Memoirs', posts: posts });
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
 module.exports = router;
