a06a517fbfb0d169d41ae88ccf0810cf11fa7a95
aaronk416 <aaron.kim91@myhunter.cuny.edu>
2026-04-01 03:58:39 -0400

    fix mongodb crash for render

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get("/", function (req, res, next) {
   try {
-    const db = req.app.locals.db;
-    // const posts = await db.collection('songs')
-    //   .find()
-    //   .toArray();
+    // TEMP FIX: no MongoDB (prevents Render crash)
 
-    const posts = await db.collection('songs')
-      .aggregate([
-        {
-          $lookup: {
-            from: 'users',
-            foreignField: '_id',
-            localField: 'authorID',
-            as: 'authors'
-          }
-        },
-        {
-          $lookup: {
-            from: 'comments',
-            foreignField: 'songID',
-            localField: '_id',
-            as: 'comments'
-          }
-        },
-        { $sort: { createdAt: -1}}
-      ])
-      .toArray();
+    const posts = [];
 
-
-    console.log(posts);
-
-    res.render('index', { title: 'Memoirs of a Melody', posts: posts });
+    res.render("index", {
+      title: "Memoirs of a Melody",
+      posts: posts,
+    });
   } catch (error) {
     console.log(error);
+    res.status(500).send("Server error");
   }
-
 });
 
 module.exports = router;
