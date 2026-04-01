4238fb40802c65d96ebd7c3a72160edac046d8e8
aaronk416 <aaron.kim91@myhunter.cuny.edu>
2026-04-01 04:06:48 -0400

    remove mongodb completely for render deployment

--- app.js
+++ app.js
@@ -1,51 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const connectToDatabase = require('./config/db');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const songsRouter = require('./routes/songs');
-const commentsRouter = require('./routes/comments');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var songsRouter = require("./routes/songs");
+var commentsRouter = require("./routes/comments");
 
 var app = express();
 
-connectToDatabase()
-  .then((db) => {
-    app.locals.db = db;
-})
+/* NO DATABASE CONNECTION (SAFE MODE) */
+app.locals.db = null;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/songs', songsRouter);
-app.use('/comments', commentsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/songs", songsRouter);
+app.use("/comments", commentsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch 404
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
