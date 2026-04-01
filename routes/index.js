b87cc5a2ae4fb42ba2dbde0c1a81630cff47df46
aaronk416 <aaron.kim91@myhunter.cuny.edu>
2026-04-01 17:50:17 -0400

    force test change

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res) {
-  res.render("index", {
-    title: "Memoirs of a Melody",
-    posts: [],
-  });
+  res.send("SITE IS WORKING");
 });
 
 module.exports = router;
