0e3ca0f42ca9c474219dd7f09405d5ff1b6df95c
aaronk416 <aaron.kim91@myhunter.cuny.edu>
2026-04-01 04:02:32 -0400

    hard remove mongodb completely

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  try {
-    // TEMP FIX: no MongoDB (prevents Render crash)
-
-    const posts = [];
-
-    res.render("index", {
-      title: "Memoirs of a Melody",
-      posts: posts,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Server error");
-  }
+router.get("/", function (req, res) {
+  res.render("index", {
+    title: "Memoirs of a Melody",
+    posts: [],
+  });
 });
 
 module.exports = router;
